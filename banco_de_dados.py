5db963a09c8c4d80ede72b90d7894e586d8677a5
anabotr <ana.botr@gmail.com>
2025-08-29 19:25:52 -0300

    Comentado banco de dados

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -3,12 +3,26 @@ Organiza as informações do csv
 """
 
 def carregar_contas_de_csv(caminho_arquivo: str) -> dict:
+    """
+    Carrega o arquivo csv para um dicionário. O arquivo não pode ser vazio.
+
+    Args:
+        caminho_arquivo (str): Caminho do arquivo que contém os dados em csv.
+
+    Returns:
+        dict: Dicionário que contém os dados devidamente organizados.
+
+    Raises:
+        FileNotFoundError: É retornado quando o caminho do arquivo não existe.
+        IndexError: É retornado quando o arquivo está com algum problema, vazio, por exemplo.
+    """    
     contas = {}
     try: 
         with open(caminho_arquivo, "r") as arquivo:
             linhas = arquivo.readlines()
             linhas = [linha.strip() for linha in linhas] 
             cabecalho = linhas[0].split(",")
+
             
             for linha in linhas[1:]:
                 colunas = linha.split(",")
@@ -19,18 +33,32 @@ def carregar_contas_de_csv(caminho_arquivo: str) -> dict:
                 contas[numero_conta] = {"cliente": cliente, "saldo" : saldo}
 
     except FileNotFoundError:
-        contas["numero_conta"] = {"cliente": None, "saldo" : None}
         print("Arquivo não encontrado")
+        contas = {}
+    except IndexError:
+        print("Erro ao ler arquivo, ele pode estar vazio")
+        contas = {}
     return contas
 
-def salvar_contas_para_csv(caminho_arquivo: str, contas: dict) -> None:
-    with open(caminho_arquivo, "w") as file:
-        file.write("numero_conta,cliente,saldo\n")
 
-        for numero_conta, info_saldo in contas.items():
-            linha = f"{numero_conta},{info_saldo['cliente']},{info_saldo['saldo']}\n"
-            file.write(linha)
+def salvar_contas_para_csv(caminho_arquivo: str, contas: dict) -> None:
+    """Abre um arquivo csv, lê as linhas, e organiza os dados e formata pro modelo de dicionário especificado.
+    Ele só executa se o arquivo recebido for não vazio.
+
+    Args:
+        caminho_arquivo (str): Caminho do arquivo csv no qual os dados serão salvos.
+        contas (dict): Dicionário a ser realocado em csv.
+    """    
+    if contas != {}:
+        with open(caminho_arquivo, "w") as file:
+            file.write("numero_conta,cliente,saldo\n")
+
+            for numero_conta, info_saldo in contas.items():
+                linha = f"{numero_conta},{info_saldo['cliente']},{info_saldo['saldo']}\n"
+                file.write(linha)
+    else:
+        print("O arquivo está vazio.")
     return 
 
-teste = carregar_contas_de_csv("C:/Users/lcone/.ipython/.vscode/lista1/contas22.csv")
-salvar_contas_para_csv("C:/Users/lcone/.ipython/.vscode/lista1/contas22.csv", teste)
+teste = carregar_contas_de_csv("Teste1_LP/test.csv")
+salvar_contas_para_csv("Teste1_LP/teste.csv", teste)
