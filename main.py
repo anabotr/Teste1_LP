a088020273981ed513d3cf8aa9969aad36905cba
lconerado2@gmail.com <lconerado2@gmail.com>
2025-08-31 17:41:04 -0300

    Alterações na main

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import operacoes_banco as ope
 
 # FALTA COMENTAR: (O QUE É CADA ARQUIVO, CONFERIR NOMENCLATURAS E RETURN)
-# FALTA REVER AS FUNÇÕES
-# SFINX 
-# GIT PUSH 
-
-
-import operacoes_banco as ope
-
-# FALTA COMENTAR: (O QUE É CADA ARQUIVO, CONFERIR NOMENCLATURAS E RETURN)
 # SFINX 
 # GIT PUSH 
 
@@ -47,7 +39,9 @@ while flag:
 
             elif resp_gerente == '2':
                 rico = ope.identificar_cliente_mais_rico()
-                print(rico)
+                print(f"O cliente mais rico é {list(rico.values())[0]}")
+                print('-'*80)
+
             elif resp_gerente == '3':
                 numero_conta = []
                 deposito = []
@@ -55,26 +49,46 @@ while flag:
                 print('-'*80)
                 conta_deposito = input("Conta: ")
                 numero_conta.append(conta_deposito)
-                deposito.append(valor_deposito)
-                print("Mais alguma conta?\n 1. SIM\n 2. NÃO")
-                resp_gerente3 = input()
-                while resp_gerente3 == '1': 
-                    conta_deposito = input("Em qual conta?")
-                    valor_deposito = input("Qual valor")
-                    numero_conta.append(conta_deposito)
+                while conta_deposito != "": 
+                    valor_deposito = ope.trata_float(input("Valor: "))
+                    print("-"*80)
                     deposito.append(valor_deposito)
-                    resp_gerente3 = input("Mais alguma conta?\n 1. SIM\n 2. NÃO")
-                if resp_gerente3 != '1':
-                    pass
-                ope.somar_saldos_em_lote(numero_conta = numero_conta, deposito = deposito)
+                    conta_deposito = input("Conta:")
+                    numero_conta.append(conta_deposito)
+                
+                numero = ope.somar_saldos_em_lote(numero_conta = numero_conta, deposito = deposito)
+                print(f"Operação realizada com sucesso, {numero} conta(s) modificadas")
+                print('-'*80)
+                
             elif resp_gerente == '4':
-                print('jesus ta tirando dinheiro')
-                fundos_rm = input()
+                numero_conta = []
+                saque = []
+                print('Insira os dados solicitados, quando acabar, deixe o campo conta vazio.')
+                print('-'*80)
+                conta_saque = input("Conta: ")
+                numero_conta.append(conta_saque)
+                while conta_saque != "": 
+                    valor_saque = ope.trata_float(input("Valor: "))
+                    print("-"*80)
+                    saque.append(valor_saque)
+                    conta_saque = input("Conta: ")
+                    numero_conta.append(conta_saque)
+                numero = ope.subtrair_saldos_em_lote(numero_conta = numero_conta, saque = saque)
+                print(f"Operação realizada com sucesso, {numero} conta(s) modificadas.")
+                print('-'*80)
+
             elif resp_gerente == '5':
                 print('Insira o número da conta:')
                 numero_conta = input()
                 print("Insira o Nome do cliente: ")
                 nome_cliente = input()
+                conta = ope.criar_conta(numero_conta, nome_cliente)
+                if conta != None: 
+                    print(f"Conta criada com sucesso! \"Numero da conta\": {conta[0]}, \"Cliente\": {conta[1]}"  )
+                    print('-'*80)
+                else: 
+                    print("Informações inválidas")
+
             elif resp_gerente == '6':
                 fgerente = False
                 print('-'*80)
@@ -86,11 +100,10 @@ while flag:
     elif resposta == "2":
         print("Digite o número da conta:")
         conta = input()
-        # verifica se a conta ta no banco de dados
-        if conta == 'false':
-            esta = False
+    
+        if ope.conta_in_bd(conta) != True: #quando deixa como true ele mostra o index value do carrega banco de dados e continua
             print("Erro: Conta inexistente.")
-
+            print('-'*80)
         else:
             print('-'*80)
             fcliente = True
@@ -108,15 +121,41 @@ while flag:
                     print("Seu saldo atual é de:" , ope.consultar_saldo(conta))
                     print('-'*80)
                 if resp_cliente == '2':
-                    print("deposita essa grana ai")
-                    valor = input()
+                    print('Insira o valor do depósito: ')
+                    valor = ope.trata_float(input())
+                    if valor != None:
+                        operacao = ope.depositar(conta, valor)
+                        total = ope.consultar_saldo(conta)
+                        print(f"{operacao[1]}! Seu saldo atual é: {total}" )
+                    else: 
+                        print('Valor inválido')
+                    print('-'*80)
+
                 if resp_cliente == '3':
-                    print('saca o dindin')
-                    valor = input()
+                    print('Insira o valor do saque: ')
+                    valor = ope.trata_float(input())
+                    if valor != None:
+                        operacao = ope.sacar(conta, valor)
+                        total = ope.consultar_saldo(conta)
+                        print(f"{operacao[1]}! Seu saldo atual é: {total}" )
+                    else: 
+                        print('Valor inválido')
+                    print('-'*80)
+
                 if resp_cliente == '4':
                     print("Insira a conta que receberá o depósito: ")
                     conta_destino = input()
-                    valor = input()
+                    if conta_destino.isnumeric() == True: 
+                        print("Insira o valor: ")
+                        valor = ope.trata_float(input())
+                        if valor == None: 
+                            print("Valor inválido")
+                        if valor != None: 
+                            tupla = ope.realizar_transferencia(conta_origem = conta, conta_destino=conta_destino, valor=valor)
+                            print(tupla[1])
+                    else: 
+                        print("Valor inválido")
+                    print('-'*80)
                 if resp_cliente == '5':
                     fcliente = False
                     print('-'*80)
@@ -124,5 +163,7 @@ while flag:
         flag = False
         print('-'*80)
     else:
-        print("Entrava inválida")
+        flag = True 
+        print("Entrada inválida, escolha novamente")
+        print('-'*80)
 
--- operacoes_banco.py
+++ operacoes_banco.py
@@ -36,7 +36,8 @@ def conta_in_bd(numero_conta: str) -> bool:
             return True
         return False
     except ValueError:
-        print("Insira uma conta válida.")
+        return False
+    
 
 def acha_indice(numero_conta:str, dados = consulta_dados()) -> int | None:
     """Encontra o índice da conta solicitada no dicionário do banco de dados.
@@ -49,14 +50,15 @@ def acha_indice(numero_conta:str, dados = consulta_dados()) -> int | None:
         int | None: Retorna o índice ou none, caso a conta não exista no banco de dados.
     """
     if conta_in_bd(numero_conta):
-        for i, (conta, nome, saldo) in enumerate(dados):
+        for indice, (conta, _, _) in enumerate(dados):
             if f"{int(numero_conta):04d}" == str(conta):
-                return i
+                return indice
     else:
         return None
 
 def trata_float(valor : str|float|int) -> str:
-    """Trata as entradas numéricas, garantindo que os valores sigam o padrão americano.
+    """Trata as entradas numéricas, garantindo que os valores sigam o padrão do banco: apenas separador decimal, sendo ele um ponto final.
+    Se a entrada for vazia, retorna 0.
 
     Args:
         string (str | float | int): Número a ser modificado, passado em forma de string, int ou float.
@@ -64,22 +66,32 @@ def trata_float(valor : str|float|int) -> str:
     Returns:
         str: Valor inserido com ponto no separamento decimal e arredondamento para duas casas.
     """
-
+    if valor == "" or valor is None:
+        return 0.0
     try:
         valor_final = float(valor)
-    except ValueError:
-        valor_final = valor[::-1].replace(',', '.', 1)[::-1]
-    return float(f"{round(float(valor_final),2):02f}")
+        return valor_final
+    except:
+        valor = valor.strip()
+        tem_letra = any(c.isalpha() for c in valor)
+        if tem_letra: 
+            return None
+        valor_final = valor[:-3].replace('.', '').replace(',', '')
+        valor_final += valor[-3:].replace(',', '.')
+        try:
+            return float(f"{round(float(valor_final),2):02f}")
+        except ValueError:
+            return None
 
-def saldo_suficiente(numero_conta: str, valor: str) -> bool:
-    """_summary_
+def saldo_suficiente(numero_conta: str, valor: str|float|int) -> bool:
+    """Verifica se a conta tem saldo suficiente para retirar algum valor específico.
 
     Args:
-        numero_conta (str): _description_
-        valor (str): _description_
+        numero_conta (str): Número da conta a ter saldo modificado.
+        valor (str | float | int): Valor a ser debitado da conta.
 
     Returns:
-        bool: _description_
+        bool: Um valor booleano, True se é possível realizar a operação e False se não.
     """
     dados_banco = consulta_dados()
     indice = acha_indice(numero_conta)        
@@ -100,7 +112,7 @@ def atualiza_saldo(numero_conta: str, valor: float):
     conta = bd.carregar_contas_de_csv()
     indice = acha_indice(numero_conta)        
     if conta_in_bd(numero_conta):
-        conta[f"{int(numero_conta):04d}"] = {"cliente": dados_banco[indice][1], "saldo": f"{valor:.2f}"}
+        conta[f"{int(numero_conta):04d}"] = {"cliente": dados_banco[indice][1], "saldo": f"{float(valor):.2f}"}
         bd.salvar_contas_para_csv(conta)
         
     return 
@@ -109,7 +121,8 @@ def atualiza_saldo(numero_conta: str, valor: float):
 #Funções solicitadas
 
 def criar_conta(numero_conta: str, nome_cliente: str) -> tuple[int,dict]:
-    """Cria uma conta, coom base no número e nome forneci
+    """Abre uma conta no banco de dados, com base no número da conta e nome fornecidos
+    .
 
     Args:
         numero_conta (str): Número da conta que será aberta.
@@ -121,13 +134,16 @@ def criar_conta(numero_conta: str, nome_cliente: str) -> tuple[int,dict]:
     """
     conta = bd.carregar_contas_de_csv()
     if not conta_in_bd(numero_conta): 
-        numero_conta = int(numero_conta)
-        conta[f"{numero_conta:04d}"] = {"cliente": nome_cliente, "saldo": float(f"{00:05.2f}")}
-        bd.salvar_contas_para_csv(conta)
-        return (int(f"{numero_conta:04d}"),  conta[f"{numero_conta:04d}"])
+        if numero_conta.isdigit():
+            numero_conta = int(numero_conta)
+            conta[f"{numero_conta:04d}"] = {"cliente": nome_cliente, "saldo": float(f"{00:05.2f}")}
+            bd.salvar_contas_para_csv(conta)
+            return (int(f"{numero_conta:04d}"),  conta[f"{numero_conta:04d}"])
+        else: 
+            return None
 
 def consultar_saldo(numero_conta: str) -> float | None:
-    """Função que retorna o saldo de uma conta específica.
+    """Função que consulta o saldo de uma conta informada no banco de dados. Contas inexistentes retornam None.
 
     Args:
         numero_conta (str): Número da conta com saldo a ser consultado.
@@ -187,27 +203,30 @@ def somar_saldos_em_lote(**kwargs) -> int:
         int: Quantidade de contas que tiveram saldo atualizado.
     """    
 
-    contas = 0
+    contar_contas = 0
     conta = bd.carregar_contas_de_csv()
     info = kwargs
-    contas = info["numero_conta"]
-    depositos = info["deposito"]
+    contas = info["numero_conta"] #lista
+    depositos = info["deposito"] #lista
 
-    for numero_conta in contas:
-        if conta_in_bd(numero_conta):      
-            indice_contas = contas.index(info)
-            indice_csv = acha_indice(numero_conta)
-            depositos_corresp = depositos[indice_contas] 
-            if float(depositos_corresp) >= 0:
-                info_colunas = conta[indice_csv]
+
+    for numero_conta in contas: #para cada item em contas vai verificar  
+        if conta_in_bd(numero_conta):    #se o número da conta existe
+            indice_contas = contas.index(numero_conta) #vai pegar o indece da lista contas
+            numero_conta = str(numero_conta).zfill(4)
+            depositos_corresp = trata_float(depositos[indice_contas])  #vai pesquisar o mesmo 
+            if depositos_corresp == None: 
+                continue
+            if depositos_corresp >= 0: # se o float for maior que zero 
+                info_colunas = conta[numero_conta] 
                 saldo_original = info_colunas["saldo"] 
-                saldo_novo = round(float(saldo_original) + float(depositos_corresp),2)
+                saldo_novo = round((float(saldo_original) + float(depositos_corresp)),2)
                 atualiza_saldo(numero_conta, saldo_novo)
-                contas += 1
+                contar_contas += 1
         else: 
             pass
     
-    return contas
+    return contar_contas
 
 def subtrair_saldos_em_lote(**kwargs) -> int:
     """Subtrai valores aos saldos de múltiplas contas de uma vez.
@@ -228,53 +247,21 @@ def subtrair_saldos_em_lote(**kwargs) -> int:
     for numero_conta in contas:
         if conta_in_bd(numero_conta):
             indice_contas = contas.index(numero_conta)
+            numero_conta = str(numero_conta).zfill(4)
             saque_corresp = saque[indice_contas]
-            if (saldo_suficiente(numero_conta, saque_corresp) and saque_corresp >= 0):
-                indice_csv = acha_indice(numero_conta)
-                saldo_original = conta[indice_csv]["saldo"] 
-                saldo_novo = round(float(saldo_original) - float(saque_corresp),2)
-                conta[indice_csv]["saldo"] = saldo_novo
-                numero_contas += 1
-                atualiza_saldo(numero_conta, saldo_novo)
-    return contas
+            if saque_corresp == None: 
+                numero_contas = 0
+                continue
+            if int(saque_corresp) >= 0:
+                if saldo_suficiente(numero_conta, saque_corresp): 
+                    indice_csv = acha_indice(numero_conta)
+                    saldo_original = conta[numero_conta]["saldo"] 
+                    saldo_novo = round((float(saldo_original) - float(saque_corresp)), 2)
+                    conta[numero_conta]["saldo"] = saldo_novo
+                    numero_contas += 1
+                    atualiza_saldo(numero_conta, saldo_novo)
+    return numero_contas
 
-def realizar_transferencia(conta_origem: str, conta_destino: str, valor: float) -> tuple[bool,str]:
-    """Realiza uma transferência entre duas contas especificadas, com base no valor dado.
-    Contas inexistentes ou saldos insuficientes serão notificados no caso de insucesso.
-
-    Args:
-        conta_origem (str): Conta de ondde o dinheiro será debitado.
-        conta_destino (str): Conta que recebrá o valor inserido.
-        valor (float): Quantidade transacinada.
-
-    Returns:
-        tuple[bool,str]: Tupla com dois elementos, o primeiro é True se a operação fucionou e False se deu errado. 
-        O segundo elemento é uma string com a descrição da operação/erro, se ocorreu.
-    """
-    conta = bd.carregar_contas_de_csv()
-    operacao = True
-    erro1 = False
-    erro2 = False
-    if conta_in_bd(conta_origem) and conta_in_bd(conta_destino):
-        indice_csv_contaorigem = acha_indice(conta_origem)
-        indice_csv_contadestino = acha_indice(conta_destino)
-        saldo_conta_origem = conta[indice_csv_contaorigem]["saldo"]
-        saldo_conta_destino = conta[indice_csv_contadestino]["saldo"]
-        if not saldo_suficiente(conta_origem, valor):
-            erro1_descrição = "Operação não realizada, saldo insuficiente"
-            tupla = (erro1, erro1_descrição)
-        else: 
-            operacao_descricao = "Operação realizada com sucesso"
-            saldo_novo_origem = round((float(saldo_conta_origem) - float(valor)),2)
-            saldo_novo_destino = (float(saldo_conta_destino) + float(valor))
-            atualiza_saldo(conta_origem, saldo_novo_origem)
-            atualiza_saldo(conta_destino, saldo_conta_destino)
-            tupla = (operacao, operacao_descricao)
-    else:
-        erro2_descricao = "Operação não realizada, conta não existe"
-        tupla = (erro2, erro2_descricao)
-
-    return tupla
 
 def depositar(numero_conta: str, valor: float) -> tuple[bool, str]:
     """Depositar o valor inserido na conta especificada.
@@ -288,21 +275,21 @@ def depositar(numero_conta: str, valor: float) -> tuple[bool, str]:
         tuple[bool,str]: Tupla na qual o primeiro elemento é um booleano que retorna True se a operação deu certo e False se não.
         O segundo elemento é uma string com a descrição da operação/erro, se ocorreu.
     """
-    conta = bd.carregar_contas_de_csv()
+    contas = bd.carregar_contas_de_csv()
     verifica = False
-    indice = acha_indice(numero_conta)
     if valor > 0:
         if conta_in_bd(numero_conta):
-            saldo_atual = conta[indice]["saldo"]
+            saldo_atual = contas[f"{int(numero_conta):04d}"]["saldo"]
             saldo_novo = round((float(saldo_atual) + float(valor)),2)
             atualiza_saldo(numero_conta, saldo_novo)
             verifica = True 
-            motivo = "Depósito concluído"
+            motivo = "Operação concluída"
         else: 
             motivo = "Erro: conta inexistente."
     else: 
-        motivo = "Impossível depositar valores negativos"
+        motivo = "Impossível operar com valores negativos"
     return (verifica, motivo)
+
 
 def sacar(numero_conta: str, valor: float) -> tuple[bool, str]:
     """Sacar o valor inserido na conta especificada.
@@ -322,19 +309,48 @@ def sacar(numero_conta: str, valor: float) -> tuple[bool, str]:
     if valor > 0:
         if saldo_suficiente(numero_conta, valor) == True:
             if conta_in_bd(numero_conta):
-                saldo_atual = conta[indice]["saldo"]
+                saldo_atual = conta[f"{int(numero_conta):04d}"]["saldo"]
                 saldo_novo = round((float(saldo_atual) - float(valor)),2)
                 atualiza_saldo(numero_conta, saldo_novo)
                 verifica = True 
-                motivo = "Saque concluído"
+                motivo = "Operação concluída"
             else: 
                 motivo = "Conta inexistente."
         else: 
             motivo = "Erro: saldo insuficiente."
     else: 
-        motivo = "Impossível sacar valores negativos"
+        motivo = "Impossível operar com valores negativos"
     return (verifica, motivo)
 
 #Descrição: Verifica se a conta existe, se o valor é positivo e se há saldo suficiente. Se tudo estiver OK, subtrai o valor do saldo.
 #Retorno: Uma tupla indicando sucesso/falha com uma mensagem apropriada (ex: (False, "Erro: saldo insuficiente.")).
 
+def realizar_transferencia(conta_origem: str, conta_destino: str, valor: float) -> tuple[bool,str]:
+    """Realiza uma transferência entre duas contas especificadas, com base no valor dado.
+    Contas inexistentes ou saldos insuficientes serão notificados no caso de insucesso.
+
+    Args:
+        conta_origem (str): Conta de ondde o dinheiro será debitado.
+        conta_destino (str): Conta que recebrá o valor inserido.
+        valor (float): Quantidade transacionada.
+
+    Returns:
+        tuple[bool,str]: Tupla com dois elementos, o primeiro é True se a operação fucionou e False se deu errado. 
+        O segundo elemento é uma string com a descrição da operação/erro, se ocorreu.
+    """
+    conta = bd.carregar_contas_de_csv()
+    operacao = True
+    if conta_in_bd(conta_origem) and conta_in_bd(conta_destino):
+        resultado = sacar(conta_origem, valor)
+        if resultado[0] == False: 
+            operacao = False
+            ope_descri = resultado[1]
+        else:
+            resultado2 = depositar(conta_destino, valor)
+            if resultado2[0] == True:
+                operacao = True
+                ope_descri = "Transferência realizada com sucesso"
+            else:
+                operacao = False
+                ope_descri = resultado2[1]
+    return (operacao, ope_descri) 
